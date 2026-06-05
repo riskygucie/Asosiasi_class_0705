664d6a0fefeece751bc4384b538791f0ccf209cb
riskygucie <riskygucie@gmail.com>
2026-06-05 15:48:32 +0700

    membuat class matakuliah, attribute, dan konstruktor

--- Orang.java
+++ Orang.java
@@ -18,3 +18,14 @@ abstract class Orang {
     // ... kode sebelumnya ...
     public abstract void tampilkanPeran();
 }
+class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+}
